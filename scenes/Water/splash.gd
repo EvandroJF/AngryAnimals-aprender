12e99ed34d89381765c6294862b2aa9996254a2b
Evandro Junio <juniok591@gmail.com>
2025-02-24 08:27:51 -0300

    create game scenes level and water

---
+++ scenes/Water/splash.gd
@@ -0,0 +1,16 @@
+extends AudioStreamPlayer
+
+@onready var splash: AudioStreamPlayer = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_water_body_entered(body: Node2D) -> void:
+	splash.play()
